1a80b28a475d82b30197b82a4d063ca832d48b53
PurinatG-01 <fame3js49@gmail.com>
2020-11-16 18:05:08 +0700

    Merge 2 feature v1.1

--- components/theme.js
+++ components/theme.js
@@ -3,9 +3,6 @@ const { ThemeConsumer } = require("styled-components");
 export const THEME = {
     primary: "rgb(74,138,240)",
     secondary: "#fefefe",
-<<<<<<< HEAD
-    black: "#000000"
-=======
-    delete: "#ff1744"
->>>>>>> 69a82aec2e58c159df7b3d53a8d7af02c92f6076
+    black: "#000000",
+    delete: "#ff1744",
 }
