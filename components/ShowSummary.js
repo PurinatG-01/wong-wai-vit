882164f0882d4bb30e259c2175ca3b7d8a2f8a3d
Maijsp <spak.flash12@gmail.com>
2020-12-03 00:25:52 +0700

    Finish mockup summary page (mockup data)

--- components/ShowSummary.js
+++ components/ShowSummary.js
@@ -4,6 +4,13 @@ import React, { useState, useEffect } from 'react'
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid'
 import Box from '@material-ui/core/Box';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 // Chart.js
 import { Pie } from 'react-chartjs-2';
@@ -48,12 +55,23 @@ const dataSeparate = {
     ]
 }
 
+function createData(productname, ok, ng, rework, resend, bend) {
+    return { productname, ok, ng, rework, resend, bend };
+}
+  
+const rows = [
+    createData('ชุบของใหม่', 159, 6, 24, 4, 5),
+    createData('ล้างชุบใหม่', 237, 9, 37, 4, 3),
+    createData('เปอร์เซ็นต์', 262, 16, 24, 6, 2),
+];
+
 export default function showSummary() {
     return (
         <>
             <TopBar>
                 <Typography variant="h3">สรุปรายการ</Typography>
             </TopBar>
+            { /* Charts */ }
             <Grid container spacing={2}>
                 <Grid item xs = {6}>
                     <Pie
@@ -116,7 +134,131 @@ export default function showSummary() {
                     </Box>
                 </Grid>
             </Grid>
-            
+            {/* Product Line: 2 */}
+            <Box m={2} pt={3}>
+                <Typography variant="h5" align="left"> Product Line: 2 </Typography>
+                    <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>รายการ</TableCell>
+                                <TableCell align="right">ของที่ใช้ได้ทั้งหมด</TableCell>
+                                <TableCell align="right">ของเสียทั้งหมด</TableCell>
+                                <TableCell align="right">ล้างชุบใหม่</TableCell>
+                                <TableCell align="right">ล้างส่งคืน</TableCell>
+                                <TableCell align="right">งานผิดรูป</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            { /* Just mockup data */ }
+                            {rows.map((row) => (
+                                <TableRow key={row.productname}>
+                                <TableCell component="th" scope="row">
+                                    {row.productname}
+                                </TableCell>
+                                    <TableCell align="right">{row.ok}</TableCell>
+                                    <TableCell align="right">{row.ng}</TableCell>
+                                    <TableCell align="right">{row.rework}</TableCell>
+                                    <TableCell align="right">{row.resend}</TableCell>
+                                    <TableCell align="right">{row.bend}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
+            { /* Product Line: 3 */ }
+            <Box m={2} pt={3}>
+                <Typography variant="h5" align="left"> Product Line: 3 </Typography>
+                    <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>รายการ</TableCell>
+                                <TableCell align="right">ของที่ใช้ได้ทั้งหมด</TableCell>
+                                <TableCell align="right">ของเสียทั้งหมด</TableCell>
+                                <TableCell align="right">ล้างชุบใหม่</TableCell>
+                                <TableCell align="right">ล้างส่งคืน</TableCell>
+                                <TableCell align="right">งานผิดรูป</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            { /* Just mockup data */ }
+                            {rows.map((row) => (
+                                <TableRow key={row.productname}>
+                                <TableCell component="th" scope="row">
+                                    {row.productname}
+                                </TableCell>
+                                    <TableCell align="right">{row.ok}</TableCell>
+                                    <TableCell align="right">{row.ng}</TableCell>
+                                    <TableCell align="right">{row.rework}</TableCell>
+                                    <TableCell align="right">{row.resend}</TableCell>
+                                    <TableCell align="right">{row.bend}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
+            { /* Sum for Product line 2 & Product line 3 */ }
+            <Box m={2} pt={3}>
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>รายการ</TableCell>
+                                <TableCell align="right">ของที่ใช้ได้ทั้งหมด</TableCell>
+                                <TableCell align="right">ของเสียทั้งหมด</TableCell>
+                                <TableCell align="right">ล้างชุบใหม่</TableCell>
+                                <TableCell align="right">ล้างส่งคืน</TableCell>
+                                <TableCell align="right">งานผิดรูป</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            { /* Just mockup data */ }
+                            <TableRow key={1}>
+                                <TableCell component="th" scope="row">รวมทั้งหมด</TableCell>
+                                <TableCell align="right">{3}</TableCell>
+                                <TableCell align="right">{4}</TableCell>
+                                <TableCell align="right">{5}</TableCell>
+                                <TableCell align="right">{6}</TableCell>
+                                <TableCell align="right">{7}</TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
+            { /* Copper Tank */}
+            <Box m={2} pt={3}>
+                <Typography variant="h5" align="left"> Copper Tank </Typography>
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>รายการ</TableCell>
+                                <TableCell align="right">SA</TableCell>
+                                <TableCell align="right">SB</TableCell>
+                                <TableCell align="right">B1</TableCell>
+                                <TableCell align="right">B2</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            { /* Just mockup data */ }
+                            {rows.map((row) => (
+                                    <TableRow key={row.productname}>
+                                    <TableCell component="th" scope="row">
+                                        {row.productname}
+                                    </TableCell>
+                                        <TableCell align="right">{row.ok}</TableCell>
+                                        <TableCell align="right">{row.ng}</TableCell>
+                                        <TableCell align="right">{row.rework}</TableCell>
+                                        <TableCell align="right">{row.resend}</TableCell>
+                                    </TableRow>
+                                ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
         </>
     )
 }
